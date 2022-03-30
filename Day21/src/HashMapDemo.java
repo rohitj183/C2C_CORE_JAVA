3e55227ad7ab87c91eef95de8368b96f10467bf9
Rohitj183 <rrj1831999@gmail.com>
2022-03-30 18:35:22 +0530

    HashMap

---
+++ Day21/src/HashMapDemo.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class HashMapDemo {
+	public static void main(String args[]) {
+	HashMap<Integer, String> hmp = new HashMap<Integer, String>();
+	//hmp.put(1,"Hello");
+	//hmp.put(2,"World");
+	hmp.put(1 , " Hello");
+	hmp.put(2 , " World");
+	System.out.println(hmp);
+	}
+}
